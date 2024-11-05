92caf8f55dbce7861516588ba6724d1f2be47666
anhhao0804 <voanhhao95@gmail.com>
2024-11-05 10:37:44 +0700

    upCode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductList from './component/ProductList';
+import ProductCreate from './component/ProductCreate';
+import 'react-toastify/dist/ReactToastify.css';
+import {ToastContainer} from "react-toastify";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>  
+        <Router>
+          <Routes>
+            <Route path="/" element={<ProductList/>}/>
+            <Route path="/create" element={<ProductCreate/>}/>
+          </Routes>
+        <ToastContainer/>
+        </Router>
+    </>
   );
 }
 
